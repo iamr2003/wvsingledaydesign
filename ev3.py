47b7282d79612e8d0db456219f7dd46870dd8cac
lerker100 <lerker100@protonmail.com>
2019-08-01 11:19:13 -0400

    re-did ev3 motor class and added moveRel and moveAbs

--- ev3.py
+++ ev3.py
@@ -2,59 +2,12 @@
 
 import os
 import pyudev
+from time import sleep
 
 # --- EV3 CLASS ---
 
 PYUDEV_CONTEXT = pyudev.Context()
 
-class motorNotFound(Exception): pass
-
-class motor:
-	def __init__(self, address):
-		devices = PYUDEV_CONTEXT.list_devices(subsystem='tacho-motor', LEGO_ADDRESS='ev3-ports:out' + str(address))
-		try: self.device = next(d for d in devices)
-		except StopIteration: raise motorNotFound(address)
-
-		self.pos = open(os.path.join(self.device.sys_path, 'position'), 'r+')
-		self.duty_cycle_sp = open(os.path.join(self.device.sys_path, 'duty_cycle_sp'), 'w', 0)
-		self.duty_cycle = open(os.path.join(self.device.sys_path, 'duty_cycle'), 'r')
-		self.max_speed = int(open(os.path.join(self.device.sys_path, 'max_speed'), 'r').read())
-		self.reset()
-
-	def reset(self):
-		self.setPos(0)
-		self.setSpeed(0)
-		self.sendCommand('run-direct')
-
-	def getPos(self):
-		self.pos.seek(0)
-		return int(self.pos.read())
-
-	def setPos(self, new_pos):
-		return self.pos.write(str(int(new_pos)))
-
-	def getSpeed(self):
-		self.duty_cycle.seek(0)
-		return int(self.duty_cycle.read())
-
-	def setSpeed(self, new_duty_cycle_sp):
-		in_max = 100
-		self.duty_cycle_sp.write(str(
-			int(min(max(new_duty_cycle_sp, -in_max), in_max))
-		))
-
-	def stoppingAction(self, newMode):
-		with open(os.path.join(self.device.sys_path, 'stop_action'), 'w') as mode:
-			mode.write(str(newMode))
-
-	def sendCommand(self, newMode):
-		with open(os.path.join(self.device.sys_path, 'command'),
-				'w') as command:
-			command.write(str(newMode))
-
-	def hold(self):
-		self.stoppingAction('hold')
-		self.sendCommand('run-to-rel-pos')
 
 class sensorNotFound(Exception): pass
 
@@ -97,6 +50,60 @@ class sensor:
 
 	def ultraDistance(self):
 		return (self.getValue() / 10)
+
+class motorNotFound(Exception): pass
+
+class motor:
+	def __init__(self, address):
+		devices = PYUDEV_CONTEXT.list_devices(subsystem='tacho-motor', LEGO_ADDRESS='ev3-ports:out' + str(address))
+		try: self.device = next(d for d in devices)
+		except StopIteration: raise motorNotFound(address)
+
+	def reset(self):
+		with open(os.path.join(self.device.sys_path, 'command'), 'w') as file:
+			file.write('reset')
+
+	def sendCommand(self, newMode):
+		with open(os.path.join(self.device.sys_path, 'command'),'w') as file:
+			file.write(str(newMode))
+
+	def getPos(self):
+		with open(os.path.join(self.device.sys_path, 'position'),'r') as file:
+			file.seek(0)
+			return file.read().replace("\n", "")
+
+	def run(self, speed):
+		with open(os.path.join(self.device.sys_path, 'duty_cycle_sp'), 'w') as file:
+			file.write(str(speed))
+		with open(os.path.join(self.device.sys_path, 'command'), 'w') as file:
+			file.write("run-direct")
+
+	def breaking(self, breaking):
+		if (breaking == True):
+			with open(os.path.join(self.device.sys_path, 'stop_action'), 'w') as file:
+				file.write('hold')
+			with open(os.path.join(self.device.sys_path, 'time_sp'), 'w') as file:
+				file.write('0')
+			with open(os.path.join(self.device.sys_path, 'command'), 'w') as file:
+				file.write('run-timed')
+		else:
+			self.reset()
+
+	def moveRel(self, distance, speed):
+		with open(os.path.join(self.device.sys_path, 'speed_sp'), 'w') as file:
+			file.write(str(speed))
+		with open(os.path.join(self.device.sys_path, 'position_sp'), 'w') as file:
+			file.write(str(distance))
+		with open(os.path.join(self.device.sys_path, 'command'), 'w') as file:
+			file.write('run-to-rel-pos')
+
+	def moveAbs(self, distance, speed):
+		with open(os.path.join(self.device.sys_path, 'speed_sp'), 'w') as file:
+			file.write(str(speed))
+		with open(os.path.join(self.device.sys_path, 'position_sp'), 'w') as file:
+			file.write(str(distance))
+		with open(os.path.join(self.device.sys_path, 'command'), 'w') as file:
+			file.write('run-to-abs-pos')
 
 
 if __name__ == "__main__":
