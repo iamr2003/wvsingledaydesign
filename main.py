6f72ca38b8b78a446254923d18dc3b255a63656c
lerker100 <lerker100@protonmail.com>
2019-08-02 19:41:11 -0400

    Added nunchuk code

--- main.py
+++ main.py
@@ -8,20 +8,37 @@ bMotor = ev3.motor('B')
 cMotor = ev3.motor('C')
 
 controller = wiiMote.wiiMote()
-controller.setClassic()
 controller.setLed(1)
 sleep(2)
 
 try:
+    controller.setNunchuk()
     while True:
-        if (controller.getClassicButtons("A")):
-            bMotor.setSpeed(50)
-            cMotor.setSpeed(-50)
+        if (controller.getNunchukButton("C")):
+            bMotor.run(-50)
+            cMotor.run(50)
+        elif (controller.getNunchukButton("Z")):
+            bMotor.run(50)
+            cMotor.run(-50)
         else:
-            speed = controller.getClassicJoistics()["leftY"] * 100
-            bMotor.setSpeed(int(speed + controller.getClassicJoistics()["rightX"] * 50))
-            cMotor.setSpeed(int(speed - controller.getClassicJoistics()["rightX"] * 50))
+            speed = controller.getNunchukJoistic()["Y"] * 100
+            bMotor.run(speed + controller.getNunchukJoistic()["X"] * 50)
+            cMotor.run(speed - controller.getNunchukJoistic()["X"] * 50)
 finally:
     bMotor.sendCommand("stop")
     cMotor.sendCommand("stop")
-    
+
+# Classic Controls
+# try:
+    # controller.setClassic()
+#     while True:
+#         if (controller.getClassicButton("A")):
+#             bMotor.run(50)
+#             cMotor.run(-50)
+#         else:
+#             speed = controller.getClassicJoistics()["leftY"] * 100
+#             bMotor.run(speed + controller.getClassicJoistics()["rightX"] * 50)
+#             cMotor.run(speed - controller.getClassicJoistics()["rightX"] * 50)
+# finally:
+#     bMotor.sendCommand("stop")
+#     cMotor.sendCommand("stop")
