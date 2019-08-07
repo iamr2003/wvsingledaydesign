7f92f562c86812e3c6693bf78a1d400de6ebbd9b
lerker100 <lerker100@protonmail.com>
2019-08-07 12:27:18 -0400

    Made file paths relitive

--- libs/installer.sh
+++ libs/installer.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sudo cp ev3.py /usr/lib/python2.7
-sudo cp wiiMote.py /usr/lib/python2.7
+sudo cp ~/wvsingledaydesign/libs/ev3.py /usr/lib/python2.7
+sudo cp ~/wvsingledaydesign/libs/wiiMote.py /usr/lib/python2.7
 echo libs imported
 sudo apt-get update
 sudo apt-get upgrade -y
