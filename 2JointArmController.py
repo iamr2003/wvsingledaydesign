2334bc9020ff7099174ce1ba4042081565457699
Ibrahim Rahman <31803896+iamr2003@users.noreply.github.com>
2019-08-08 15:07:32 -0400

    adjusted constants to deal with physical changes
    closest thing to functional so far

--- 2JointArmController.py
+++ 2JointArmController.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python2
 
 MAX_SPEED = 200
-
+closeClaw = True
+armZeroed = False
 # --- Library Includes ---
 
 # evdev for joysticks
@@ -42,7 +43,6 @@ def find_joystick(name):
     for abs_id, abs_info in JOYSTICK.capabilities()[ecodes.EV_ABS]:
         JOYSTICK_CAPS[abs_id] = abs_info
         JOYSTICK_STATE[abs_id] = JOYSTICK_DEFAULT_STATE
-
 def watch_joystick():
     for event in JOYSTICK.read_loop():
         #print(event.code)
@@ -50,25 +50,35 @@ def watch_joystick():
         #313/5 for press and release of right trigger
         #ABS_RZ =5 
         #ABS_Z=2
+        global closeClaw
+        global armZeroed
+        if event.type == ecodes.EV_KEY:
+            if event.code == ecodes.BTN_B:
+                if event.value == 1:
+                    closeClaw = not closeClaw
+            #rezero button
+            if event.code == ecodes.BTN_A:
+                if event.value == 1:
+                    armZeroed = False
         if event.type == ecodes.EV_ABS:
             caps = JOYSTICK_CAPS[event.code]
             JOYSTICK_STATE[event.code] = map_range(caps.min, caps.max, -1.0, 1.0, event.value)
             if abs(JOYSTICK_STATE[event.code])<0.05:JOYSTICK_STATE[event.code] = 0
 import math as m
-armZeroed = False
+
 def armController(x,y,shoulder,elbow,shoulderLimit,elbowLimit):
-    a1=11.5
-    a2=8
+    a1=8.5
+    a2=6
     global armZeroed
     if armZeroed:
         q2 = int(-(m.degrees(m.acos(((x**2) + (y**2) -(a1**2)-(a2**2))/(2*a1*a2)))))
         q1 = int(m.degrees(m.atan2(y,x)+m.atan2((a2*m.sin(q2)),(a1+a2*m.cos(q2)))))
         #print('q1(shoulder) ='+str(q1) + ' q2(elbow) =' + str(q2))
-        elbow.moveAbs(5*(180+q2),200)
-        shoulder.moveAbs(5*(q1-90),200)
+        elbow.moveAbs(5*(180+q2),100)
+        shoulder.moveAbs(int(8.33*(90-q1))-16,100)
     else:
         elbow.run(-50)
-        shoulder.run(50)
+        shoulder.run(-50)
         if elbowLimit.getValue()==1 and shoulderLimit.getValue()==1:
             elbow.reset()
             shoulder.reset()
@@ -80,6 +90,8 @@ def armController(x,y,shoulder,elbow,shoulderLimit,elbowLimit):
 if __name__ == '__main__':
     print('Initializing motors')
     claw = ev3.motor('A')
+    claw.reset()
+    claw.braking(True)
     elbow = ev3.motor('B')
     shoulder = ev3.motor('C')
     turret = ev3.motor('D')
@@ -98,11 +110,11 @@ if __name__ == '__main__':
 
     print('Starting drive loop')
     x=4
-    y=4
+    y=6
     try:
         while True:
             ls_y = -JOYSTICK_STATE[ecodes.ABS_Y]
-            rs_y = -JOYSTICK_STATE[ecodes.ABS_RY]
+            ls_x = -JOYSTICK_STATE[ecodes.ABS_X]
             rt = 1+JOYSTICK_STATE[ecodes.ABS_RZ]
             lt = 1+JOYSTICK_STATE[ecodes.ABS_Z]
             if rt !=0 and lt !=0: 
@@ -111,17 +123,19 @@ if __name__ == '__main__':
                 turret.run(int((rt-lt))*MAX_SPEED)#lazy logic to avoid comparators
             armController(x,y,shoulder,elbow,shoulderLimit,elbowLimit)
             print ('x =' + str(x) + ' y ='+ str(y))
-            x+=ls_y*.1
-            y+=rs_y*.1
-            if x>7:x=7
-            if y>10:y=10
+            x+=ls_x*.05
+            y+=ls_y*.05
+            if x>5:x=5
+            if y>8:y=8
+            if y<4:y=4
+
+            if closeClaw:
+                claw.moveAbs(0,100)
+            else:
+                claw.moveAbs(180,100)
 
     finally:
-        claw.braking(False)
-        claw.run(0)
-        elbow.braking(False)
-        elbow.run(0)
-        shoulder.braking(False)
-        shoulder.run(0)
-        turret.braking(False)
-        turret.run(0)
+        claw.reset()
+        elbow.reset()
+        shoulder.reset()
+        turret.reset()
